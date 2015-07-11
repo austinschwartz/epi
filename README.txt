0ade352db63c6f1c8f92696c09bad73eb822ed2e
schwar12 <schwar12@purdue.edu>
2015-07-10 22:06:15 -0500

    Readme

---
+++ README.txt
@@ -0,0 +1,23 @@
+Elements of Programming Interviews problems
+
+ch05 - 0 / 15
+ch06 - 0 / 26
+ch07 - 0 / 14
+ch08 - 0 / 19
+ch09 - 0 / 15
+ch10 - 0 / 17
+ch11 - 0 / 10
+ch12 - 0 / 16
+ch13 - 0 / 18
+ch14 - 0 / 17
+ch15 - 0 / 22
+ch16 - 0 / 16
+ch17 - 0 / 23
+ch18 - 0 / 14
+ch19 - 0 / 12
+ch20 - 0 / 14
+ch21 - 0 / 16
+
+Total
+0 / 284 problems
+
---
+++ count.sh
@@ -0,0 +1,22 @@
+totals=(15 26 14 19 15 17 10 16 18 17 22 16 23 14 12 14 16)
+chapters=(05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21)
+name=README.txt
+rm ${name}
+echo "Elements of Programming Interviews problems\n" >> ${name}
+for (( i=0; i<=16; i++  ))
+do
+  x=`echo */*/* | tr " " "\n" | grep  -E "((ch${chapters[$i]}.*)(java|c|cpp|txt)$)" | wc -l | bc`
+  echo "ch${chapters[$i]} - ${x} / ${totals[$i]}" >> ${name}
+done
+
+
+total=0
+for i in ${totals[@]}; do
+  let total+=$i
+done
+
+echo "\nTotal" >> ${name}
+echo */*/* | tr ' ' '\n' | grep -E "((\w*)(java|c|cpp|txt)$)" | wc -l | bc | tr -d "\n" >> ${name}
+echo " / ${total} problems\n" >> ${name}
+
+cat ${name}
